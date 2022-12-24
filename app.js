2e54fa8dddcb907cfd48f15f03bd2830866c88ef
kmgeeta157 <83817198+kumarigeeta157@users.noreply.github.com>
2022-12-24 17:45:53 +0530

    Add files via upload
    this is first commit.

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement('h1', {}, 'Namaste Everyone!');
+const heading2 = React.createElement('h2', {}, 'Namaste everybody!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
